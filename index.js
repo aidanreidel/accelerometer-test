f175ab7b3cfcb1bf2996d1ff77a68f3b3633e374
evanhackett <evanwhackett@gmail.com>
2017-07-29 15:41:21 -0700

    improved readability by padding and styling

--- index.js
+++ index.js
@@ -8,12 +8,35 @@ const accel_x = document.getElementById('accel-x')
 const accel_y = document.getElementById('accel-y')
 const accel_z = document.getElementById('accel-z')
 
-function motion(event) {
-  accel_x.innerHTML = `x-axis: ${event.acceleration.x}`
-  accel_y.innerHTML = `y-axis: ${event.acceleration.y}`
-  accel_z.innerHTML = `z-axis: ${event.acceleration.z}`
+const leftPad = (s, c, n) =>{ s = s.toString(); c = c.toString(); return s.length > n ? s : c.repeat(n - s.length) + s; }
 
-  if (!accel_x.innerHTML) {
-    accel_x.innerHTML = "No accelerometer data"
+function motion(event) {
+  if (!event.acceleration.x) {
+    accel_x.innerHTML = 'Failed to obtain accelerometer data'
+    accel_y.innerHTML = ''
+    accel_z.innerHTML = ''
+    return
   }
+
+  if (tooFast(Date.now())) {
+    return;
+  }
+
+  accel_x.innerHTML = `x-axis: ${formatData(event.acceleration.x)} m/s<sup>2</sup>`
+  accel_y.innerHTML = `y-axis: ${formatData(event.acceleration.y)} m/s<sup>2</sup>`
+  accel_z.innerHTML = `z-axis: ${formatData(event.acceleration.z)} m/s<sup>2</sup>`
+}
+
+function formatData(data) {
+  if (data) {
+    return leftPad(data.toFixed(2), ' ', 8)
+  } else {
+    return null
+  }
+}
+
+// used to slow down the text rendering on motion event
+function tooFast(date) {
+  console.log(date)
+  return date % 2 !== 0
 }
